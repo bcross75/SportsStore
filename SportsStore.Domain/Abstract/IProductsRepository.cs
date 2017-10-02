78e51a0e0ef65a5096948d7a177ef0bb89b71600
bcross75 <bcross75@yahoo.com>
2017-10-02 13:27:16 -0500

    initial commit

---
+++ SportsStore.Domain/Abstract/IProductsRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using SportsStore.Domain.Entities;
+namespace SportsStore.Domain.Abstract
+{
+    public interface IProductsRepository
+    {
+        IEnumerable<Product> Products { get; }
+    }
+}
